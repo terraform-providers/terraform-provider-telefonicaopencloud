d9cc936d72aca6131217097e61bc2082ca7e14e6
d-apurva <apurva.dhoble@click2cloud.net>
2018-09-27 16:28:01 +0530

    add csbs backup and policy resource

--- telefonicaopencloud/config.go
+++ telefonicaopencloud/config.go
@@ -493,3 +493,10 @@ func (c *Config) sfsV2Client(region string) (*golangsdk.ServiceClient, error) {
 		Availability: c.getHwEndpointType(),
 	})
 }
+
+func (c *Config) csbsV1Client(region string) (*golangsdk.ServiceClient, error) {
+	return huaweisdk.NewCSBSService(c.HwClient, golangsdk.EndpointOpts{
+		Region:       c.determineRegion(region),
+		Availability: c.getHwEndpointType(),
+	})
+}
